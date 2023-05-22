85f2eabaf2b2ce44a108e9d931107e168099c9f7
AND3FELIPE <afsilva6236@misena.edu.co>
2023-05-22 03:27:25 -0500

    Se realiza login del proyecto con implemenatcion de framework front end

---
+++ forms/login/login.js
@@ -0,0 +1,13 @@
+document.getElementById("registrer").addEventListener("click", 
+function(){
+    window.location.href="../registrer/registre.html"
+}); 
+
+document.getElementById("sitio-web-sena").addEventListener("click", function(){
+    window.location.href="https://sena.territorio.la/cms/index.php"
+});
+
+document.getElementById("sitio-web-asiste").addEventListener("click",function(){
+    window.location.href="https://sena.territorio.la/cms/index.php"
+});
+
