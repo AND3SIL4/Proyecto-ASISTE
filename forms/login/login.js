fb45acd0c4ad96c18b92d0f954e8bed22f5860f4
AND3FELIPE <afsilva6236@misena.edu.co>
2023-05-25 21:34:20 -0500

    Navegacion entre paginas

---
+++ forms/login/login.js
@@ -0,0 +1,14 @@
+document.getElementById("sitio-web-asiste").addEventListener("click", function(){
+  window.location.href="https://github.com/AND3SIL4/Proyecto-ASISTE"
+});
+document.getElementById("sitio-web-sena").addEventListener("click", function(){
+  window.location.href="https://sena.territorio.la/cms/index.php"
+});
+document.getElementById("registro-usuarios").addEventListener("click", function(){
+  // alert("Regristro de usuarios...")
+  window.location.href="../registrer/aprendices/registre.html"
+})
+document.getElementById("inicio-sesion").addEventListener("click", function(){
+  alert("Inicio de sesion de usuarios...")
+  window.location.href="https://github.com/AND3SIL4/Proyecto-ASISTE"
+});
--- forms/registrer/aprendices/register.js
+++ forms/registrer/aprendices/register.js
@@ -1,3 +1,3 @@
-document.getElementById("btn").addEventListener("click", function(){
-    window.location.href="../login/login.html"
-})
+document.getElementById("registro-usuarios").addEventListener("click", function(){
+    window.location.href="http://127.0.0.1:5500/forms/login/login.html"
+});
